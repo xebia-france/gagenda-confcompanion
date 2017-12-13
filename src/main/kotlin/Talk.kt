52aa0d02a55c1d8135b433f3e65a7cfc6e9b2e5a
Michael Ohayon <mohayon75@gmail.com>
2017-12-13 12:26:56 +0100

    Added handson type orthograph Handle day meetings

--- src/main/kotlin/Talk.kt
+++ src/main/kotlin/Talk.kt
@@ -1,18 +1,23 @@
 import java.util.*
 import java.util.regex.Pattern
 
-data class Talk(val fromTime: Date, val id: String, val toTime: Date, val title: String, val speakers: List<SpeakerTalk>, val summary: String?, val room: String?) {
+data class Talk(val fromTime: Date, val id: String, val toTime: Date, var title: String, val speakers: List<SpeakerTalk>? = null, val summary: String?, val room: String?) {
 
     private var type: String = ""
+    private var kind: String? = null
 
     init {
-        if (Pattern.compile(".*(handson|codelab).*").matcher(title.toLowerCase()).matches()) {
+        if (Pattern.compile(".*(handson|codelab|hands'on).*").matcher(title.toLowerCase()).matches()) {
             type = "Hands'On"
         } else if (Pattern.compile(".*(workshop).*").matcher(title.toLowerCase()).matches()) {
             type = "Workshop"
         } else {
             type = "Talk"
         }
-    }
 
+        if (Pattern.compile(".*(plénière|tisanes).*").matcher(title.toLowerCase()).matches()) {
+            type = "keynote"
+            kind = "keynote"
+        }
+    }
 }
