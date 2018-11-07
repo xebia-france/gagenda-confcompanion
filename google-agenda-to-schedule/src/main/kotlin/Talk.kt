4b487e55132548c73279c9cfe7f95306b979c9c2
Benjamin Lacroix <benjlacroix@gmail.com>
2018-11-07 14:20:21 +0100

    feat(track): add track if hash found

--- google-agenda-to-schedule/src/main/kotlin/Talk.kt
+++ google-agenda-to-schedule/src/main/kotlin/Talk.kt
@@ -1,18 +1,24 @@
 import java.util.*
 import java.util.regex.Pattern
 
-data class Talk(val conferenceId: String, val fromTime: Date, val id: String, val toTime: Date, var title: String, val speakers: List<SpeakerTalk>? = null, val summary: String?, var room: String?) {
+data class Talk(val conferenceId: String,
+                val fromTime: Date,
+                val id: String,
+                val toTime: Date,
+                var title: String,
+                val speakers: List<SpeakerTalk>? = null,
+                val summary: String?,
+                var room: String?) {
 
     var type: String = ""
     var kind: String? = null
+    var track: String? = null
 
     init {
-        if (Pattern.compile(".*(handson|codelab|hands'on|hands on).*").matcher(title.toLowerCase()).matches()) {
-            type = "Hands'On"
-        } else if (Pattern.compile(".*(workshop).*").matcher(title.toLowerCase()).matches()) {
-            type = "Workshop"
-        } else {
-            type = "Talk"
+        type = when {
+            Pattern.compile(".*(handson|codelab|hands'on|hands on).*").matcher(title.toLowerCase()).matches() -> "Hands'On"
+            Pattern.compile(".*(workshop).*").matcher(title.toLowerCase()).matches() -> "Workshop"
+            else -> "Talk"
         }
 
         if (Pattern.compile(".*(closing|evening party|lunch|afternoon break|morning break|breakfast|plénière|tisanes|annonce|^fondations|new comers|déjeuner|newcomers|after xke$|^welcoming|^opening|keynote|^break$|^lunch|^cocktail|^pause).*").matcher(title.toLowerCase()).matches()) {
@@ -23,6 +29,23 @@ data class Talk(val conferenceId: String, val fromTime: Date, val id: String, va
         if (title.toLowerCase().indexOf("pitch") == 0 || title.toLowerCase().indexOf("pitch") == title.toLowerCase().length - "pitch".length) {
             type = "keynote"
             kind = "keynote"
+        }
+
+        if (summary != null) {
+            when {
+                summary.contains("#rex", true) -> type = "REX"
+                summary.contains("#hands-on", true) -> type = "Hands'On"
+
+                summary.contains("#devops", true) -> track = "DevOps"
+                summary.contains("#front", true) -> track = "Front"
+                summary.contains("#agile", true) -> track = "Agile"
+                summary.contains("#back", true) -> track = "Back"
+                summary.contains("#data", true) -> track = "Data"
+                summary.contains("#mobile", true) -> track = "Mobile"
+                summary.contains("#cloud", true) -> track = "Cloud"
+                summary.contains("#iot", true) -> track = "IoT"
+                summary.contains("#craft", true) -> track = "Craft"
+            }
         }
     }
 }
